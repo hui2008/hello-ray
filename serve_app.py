276ebe6395b49aed0017b7cf9b1fb94de798c344
Hui Jiang <hui.h.jiang@hotmail.com>
2025-03-31 08:06:10 +1100

    minimal one

--- serve_app.py
+++ serve_app.py
@@ -1,17 +1,8 @@
 from ray import serve
-import ray
-from fastapi import FastAPI
-
-app = FastAPI()
-
 @serve.deployment
-@serve.ingress(app)
 class HelloWorld:
-    @app.get("/hello")
-    async def hello(self):
+    async def __call__(self):
         return "Hello from Ray Serve!"
 
 if __name__ == "__main__":
-    ray.init(address="auto")
-    serve.start(http_options={"host": "0.0.0.0"})
-    serve.run(HelloWorld.bind(), blocking=True, route_prefix="/")
+    serve.run(HelloWorld.bind(), blocking=True, route_prefix="/hello")
