c18fda45c23a402f0033dcb60a215f2e6c5f6a70
Hui Jiang <hui.h.jiang@hotmail.com>
2025-03-31 08:14:30 +1100

    join existing Ray cluster

--- serve_app.py
+++ serve_app.py
@@ -1,8 +1,12 @@
+import ray
 from ray import serve
+
 @serve.deployment
 class HelloWorld:
     async def __call__(self):
         return "Hello from Ray Serve!"
 
 if __name__ == "__main__":
+    ray.init(address="auto")
+    serve.start(http_options={"host": "0.0.0.0"})
     serve.run(HelloWorld.bind(), blocking=True, route_prefix="/hello")
